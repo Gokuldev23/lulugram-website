f7df8a28fef4113eed58b2034b72fe3cbc600dac
Gokuldev23 <gkforgeneral@gamil.com>
2025-01-23 09:02:25 +0530

    projects added

--- src/lib/Components/Hero.svelte
+++ src/lib/Components/Hero.svelte
@@ -1,5 +1,6 @@
-
-
+<script>
+    import Icon from "@iconify/svelte";
+</script>
 
 <section class="p-4">
     <h1 class="text-xl mt-16 text-blue-600 text-center font-semibold font-[Montserrat]">
@@ -16,4 +17,71 @@
         is at the heart of everything we do. We work closely with our clients to understand their needs. Thank you for considering Lulugram as your 
         growing partner. We look forward to the opportunity to collaborate with you and create a brighter future together.
     </p>
+
+    <div class="max-w-6xl mx-auto grid grid-cols-3 gap-6 border p-4 mt-20">
+        <div class="border-2 border-slate-300 rounded-xl w-full max-w-xl p-4">
+            <img class="w-32 mx-auto" src="/apps-logo/gramLogo.png" alt="">
+            <p class="text-center text-xl font-semibold mb-6">Gramlok</p>
+            <div class="border rounded-xl p-2">
+                <p class="text-center underline underline-offset-2  mb-4">Available on </p>
+                <div class="text-center  grid grid-cols-3 gap-4  items-start">
+                    <a href="/web" class="text-center text-blue-400">
+                        <Icon class="mx-auto mb-2" icon="streamline:web-solid" width="20" height="20" />
+                        <span class="text-slate-600">Web</span>
+                    </a>
+                    <a href="/android" class="text-center text-green-600">
+                        <Icon class="mx-auto mb-2" icon="fa:android" width="20" height="20" />
+                        <span class="text-slate-700">Android</span>
+                    </a>
+                    <a href="/ios" class="text-center text-blue-600">
+                        <Icon class="mx-auto mb-2" icon="fa-brands:app-store-ios" width="20" height="20" />
+                        <span class="text-slate-600">IOS</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="border-2 border-slate-300 rounded-xl w-full max-w-xl p-4">
+            <img class="w-32 mx-auto" src="/apps-logo/gramLogo.png" alt="">
+            <p class="text-center text-xl font-semibold mb-6">Gramlok</p>
+            <div class="border rounded-xl p-2">
+                <p class="text-center underline underline-offset-2  mb-4">Available on </p>
+                <div class="text-center  grid grid-cols-3 gap-4  items-start">
+                    <a href="#" class="text-center text-blue-400">
+                        <Icon class="mx-auto mb-2" icon="streamline:web-solid" width="20" height="20" />
+                        <span class="text-slate-600">Web</span>
+                    </a>
+                    <a href="#" class="text-center text-green-600">
+                        <Icon class="mx-auto mb-2" icon="fa:android" width="20" height="20" />
+                        <span class="text-slate-700">Android</span>
+                    </a>
+                    <a href="#" class="text-center text-blue-600">
+                        <Icon class="mx-auto mb-2" icon="fa-brands:app-store-ios" width="20" height="20" />
+                        <span class="text-slate-600">IOS</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="border-2 border-slate-300 rounded-xl w-full max-w-xl p-4">
+            <img class="w-32 mx-auto" src="/apps-logo/gramLogo.png" alt="">
+            <p class="text-center text-xl font-semibold mb-6">Gramlok</p>
+            <div class="border rounded-xl p-2">
+                <p class="text-center underline underline-offset-2  mb-4">Available on </p>
+                <div class="text-center  grid grid-cols-3 gap-4  items-start">
+                    <a href="#" class="text-center text-blue-400">
+                        <Icon class="mx-auto mb-2" icon="streamline:web-solid" width="20" height="20" />
+                        <span class="text-slate-600">Web</span>
+                    </a>
+                    <a href="#" class="text-center text-green-600">
+                        <Icon class="mx-auto mb-2" icon="fa:android" width="20" height="20" />
+                        <span class="text-slate-700">Android</span>
+                    </a>
+                    <a href="#" class="text-center text-blue-600">
+                        <Icon class="mx-auto mb-2" icon="fa-brands:app-store-ios" width="20" height="20" />
+                        <span class="text-slate-600">IOS</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </section>

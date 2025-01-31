67cf1dbbe2fc9f2408cc0d19c60288c58a7c8bda
Gokuldev23 <gkforgeneral@gmail.com>
2025-01-31 12:38:50 +0530

    if not authendicate in dashboard while reloads user will redirect to login page

--- src/routes/marketing/agent-dashboard/+page.svelte
+++ src/routes/marketing/agent-dashboard/+page.svelte
@@ -1,8 +1,15 @@
 <script>
+	import { goto } from "$app/navigation";
+	import { agentStore } from "$lib/stores/marketing/agentStore";
+    
     import UserHome from "$lib/Components/Dashboard/UserHome.svelte";
 
 
-    
+    $effect(()=>{
+        if(!$agentStore.signedIn){
+            goto('/marketing/auth/login')
+        }
+    })
 </script>
 
 <main>

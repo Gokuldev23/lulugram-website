e37400ec0cf93bb5a725f0ad4d41904d24109c50
Gokuldev23 <gkforgeneral@gmail.com>
2025-01-31 12:44:09 +0530

    changed to onMount for better consistency

--- src/routes/marketing/agent-dashboard/+page.svelte
+++ src/routes/marketing/agent-dashboard/+page.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
+
 	import { agentStore } from "$lib/stores/marketing/agentStore";
-    
     import UserHome from "$lib/Components/Dashboard/UserHome.svelte";
 
 
-    $effect(()=>{
+    onMount(()=>{
         if(!$agentStore.signedIn){
             goto('/marketing/auth/login')
         }
